1454a98d695aa386af2deb92fbf168146fffc060
PHOENIXIT\ekanarskaya <ekanarskaya@fil-it.ru>
2024-08-15 11:09:32 +0300

    add sections

--- intern/src/js/main.js
+++ intern/src/js/main.js
@@ -2,8 +2,9 @@ import { WOW } from './vendor/wow.min';
 
 const wow = new WOW();
 document.addEventListener('DOMContentLoaded', () => {
-  openMenu();
   wow.init();
+  openMenu();
+  handleFaqOpening();
 });
 
 function openMenu() {
@@ -12,5 +13,34 @@ function openMenu() {
   burgerIcon.addEventListener('click', () => {
     navMenu.classList.toggle('_opened');
     burgerIcon.classList.toggle('_opened');
+  });
+}
+
+function setActive(arr) {
+  const activeClassName = 'active';
+  arr.forEach((el) => {
+    const itemText = el.querySelector('.item__text');
+    if (el.classList.contains(activeClassName)) {
+      itemText.style.transition = 'none';
+      el.classList.remove(activeClassName);
+    }
+  });
+}
+
+function handleFaqOpening() {
+  const itemsList = document.querySelectorAll('.openup-list__item');
+  const activeClassName = 'active';
+  itemsList.forEach((item) => {
+    item.addEventListener('click', () => {
+      const itemText = item.querySelector('.item__text');
+      if (item.classList.contains(activeClassName)) {
+        itemText.style.transition = 'none';
+        item.classList.remove(activeClassName);
+      } else {
+        setActive(itemsList);
+        itemText.style.transition = '0.2s ease-in-out';
+        item.classList.add(activeClassName);
+      }
+    });
   });
 }
